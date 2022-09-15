b5f21d8d3f9e93a4ae08aff4e6b4314fc0eb5a3d
wb <78023776@qq.com>
2022-09-15 11:41:20 +0800

    no message

--- Sources/Saber/Utils/Log.swift
+++ Sources/Saber/Utils/Log.swift
@@ -1,46 +1,43 @@
 import Foundation
 
-public enum Log {
-    /// 是否输出到文件
-    static var isOut2File: Bool = false
-}
+public enum Log {}
 
 // MARK: - 方法
 public extension Log {
     /// 调试
-    static func debug(_ message: @autoclosure () -> String?,
+    static func debug(_ message: Any...,
                       file: String = #file,
                       line: Int = #line,
                       function: String = #function)
     {
-        self.log(level: .debug, message: message(), file: file, line: line, function: function)
+        self.log(level: .debug, message: message, file: file, line: line, function: function)
     }
 
     /// 信息
-    static func info(_ message: @autoclosure () -> String?,
+    static func info(_ message: Any...,
                      file: String = #file,
                      line: Int = #line,
                      function: String = #function)
     {
-        self.log(level: .debug, message: message(), file: file, line: line, function: function)
+        self.log(level: .debug, message: message, file: file, line: line, function: function)
     }
 
     /// 警告
-    static func warning(_ message: @autoclosure () -> String?,
+    static func warning(_ message: Any...,
                         file: String = #file,
                         line: Int = #line,
                         function: String = #function)
     {
-        self.log(level: .debug, message: message(), file: file, line: line, function: function)
+        self.log(level: .debug, message: message, file: file, line: line, function: function)
     }
 
     /// 错误
-    static func error(_ message: @autoclosure () -> String?,
+    static func error(_ message: Any...,
                       file: String = #file,
                       line: Int = #line,
                       function: String = #function)
     {
-        self.log(level: .debug, message: message(), file: file, line: line, function: function)
+        self.log(level: .debug, message: message, file: file, line: line, function: function)
     }
 }
 
@@ -55,34 +52,25 @@ extension Log {
     ///   - function: 方法
     private static func log(
         level: Log.Level,
-        message: String?,
+        message: Any...,
         file: String,
         line: Int,
         function: String)
     {
         #if DEBUG
-            // 当时时间
-            let dateStr = Date().format("HH:mm:ss.SSS", isGMT: false)
-            // 文件名称
-            let fileName = (file as NSString).lastPathComponent
-
-            let content = "\(level.levelIcon)\(level.levelName)[\(dateStr)][\(fileName) => \(function)]\(line): \(message ?? "")"
-            print(content)
-        #endif
-        // 写入文件
-        write2File(content: content)
-    }
-
-    /// 把内容写入到文件
-    /// - Parameter content: 内容
-    private static func write2File(content: String) {
-        guard isOut2File else {
-            return
+        // 输出内容
+        var content = ""
+        for item in message {
+            content += "\(item)"
         }
+        // 当时时间
+        let dateStr = Date().format("HH:mm:ss.SSS", isGMT: false)
+        // 文件名称
+        let fileName = (file as NSString).lastPathComponent
 
-        // 将内容同步写到文件中去(Caches文件夹下)
-        let fileURL = "/log.txt".urlByCache
-        FileManager.appendString(content, to: fileURL)
+        content = "\(level.levelIcon)\(level.levelName)[\(dateStr)][\(fileName) => \(function)]\(line): " + content
+        print(content)
+        #endif
     }
 }
 
