967dab7a071bd1af4ac6d798e9d8f2ef0d91eede
wb <78023776@qq.com>
2022-09-16 17:04:58 +0800

    no message

--- Sources/Saber/Shared/SaberExt.swift
+++ Sources/Saber/Shared/SaberExt.swift
@@ -2,9 +2,8 @@
 public struct SaberExt<Base> {
     /// 被包装的原始对象
     let base: Base?
-    /// 非可选构造方法
+    
     fileprivate init(_ base: Base) { self.base = base }
-    /// 可选构造方法
     fileprivate init(_ base: Base?) { self.base = base }
     
     /// 被包装的原始对象
@@ -16,8 +15,6 @@ public struct SaberExt<Base> {
 // MARK: - 需要使用的类型遵守此协议
 public protocol Saberable {}
 public extension Saberable {
-    /// 非可选类型
     var sb: SaberExt<Self> { return SaberExt(self) }
-    /// 可选类型
     static var sb: SaberExt<Self>.Type { return SaberExt<Self>.self }
 }
