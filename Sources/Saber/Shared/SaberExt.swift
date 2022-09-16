e5f2edc26e1e99a1d4110ceb34020fbd7e989064
wb <78023776@qq.com>
2022-09-16 16:18:32 +0800

    no message

--- Sources/Saber/Shared/SaberExt.swift
+++ Sources/Saber/Shared/SaberExt.swift
@@ -1,22 +1,34 @@
 // MARK: - 调用对象实体包装类型
 public struct SaberExt<Base> {
     /// 调用对象实体
-    let base: Base?
+    let base: Base
     /// 非可选构造
-    init(_ base: Base) { self.base = base }
-    /// 可选构造
-    init(_ base: Base?) { self.base = base }
+    fileprivate init(_ base: Base) {self.base = base}
+//    /// 可选构造
+//    fileprivate init(_ base: Base?) {self.base = base}
+}
+// MARK: - 开放方法/属性
+public extension SaberExt {
+    func instance() -> Base {
+        return self.base
+    }
 }
 
 // MARK: - 需要使用的类型遵守此协议
 public protocol Saberable {}
+// MARK: - 开放方法/属性
 public extension Saberable {
-    /// 静态类型
-    static var sb: SaberExt<Self>.Type { return SaberExt<Self>.self }
     /// 实例类型
     var sb: SaberExt<Self> { return SaberExt(self) }
+    /// 静态类型
+    static var sb: SaberExt<Self>.Type { return SaberExt<Self>.self }
 }
 
+// MARK: - 可使用sb扩展的类型(具体扩展在单独文件中实现)
+extension String: Saberable {}
+extension Int: Saberable {}
+extension Float: Saberable {}
+extension Double: Saberable {}
 
 
 public extension SaberExt where Base == String {
@@ -29,7 +41,32 @@ public extension SaberExt where Base == String {
     }
 }
 
-extension String: Saberable {}
-extension Int: Saberable {}
-extension Float: Saberable {}
-extension Double: Saberable {}
+public extension SaberExt where Base == Int {
+    
+    func test1111() {
+        Log.info("info...")
+        Log.debug("debug...")
+        Log.warning("warning...")
+        Log.error("error...")
+    }
+}
+
+public extension SaberExt where Base == Float {
+    
+    func test1111() {
+        Log.info("info...")
+        Log.debug("debug...")
+        Log.warning("warning...")
+        Log.error("error...")
+    }
+}
+
+public extension SaberExt where Base == Double {
+    
+    func test1111() {
+        Log.info("info...")
+        Log.debug("debug...")
+        Log.warning("warning...")
+        Log.error("error...")
+    }
+}
