2c38dfdd0b3150e36a42a50e33120642988e526a
wb <78023776@qq.com>
2022-09-16 17:22:07 +0800

    no message

--- Sources/Saber/Shared/SaberExt.swift
+++ Sources/Saber/Shared/SaberExt.swift
@@ -1,15 +1,8 @@
 // MARK: - 调用对象实体包装类型
 public struct SaberExt<Base> {
-    /// 被包装的原始对象
-    public let base: Base
-    
-    fileprivate init(_ base: Base) { self.base = base }
-//    fileprivate init(_ base: Base?) { self.base = base }
-    
-    /// 被包装的原始对象
-    public func unpack() -> Base {
-        return self.base
-    }
+    let base: Base
+    init(_ base: Base) { self.base = base }
+    func unpack() -> Base {return self.base}
 }
 
 // MARK: - 需要使用的类型遵守此协议
