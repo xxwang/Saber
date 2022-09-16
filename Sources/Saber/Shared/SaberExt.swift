dfde6079643cec09009b276ffa3c807e45f7a518
wb <78023776@qq.com>
2022-09-16 17:12:23 +0800

    no message

--- Sources/Saber/Shared/SaberExt.swift
+++ Sources/Saber/Shared/SaberExt.swift
@@ -1,13 +1,13 @@
 // MARK: - 调用对象实体包装类型
 public struct SaberExt<Base> {
     /// 被包装的原始对象
-    public let base: Base?
+    let base: Base
     
     fileprivate init(_ base: Base) { self.base = base }
-    fileprivate init(_ base: Base?) { self.base = base }
+//    fileprivate init(_ base: Base?) { self.base = base }
     
     /// 被包装的原始对象
-    public func unpack() -> Base? {
+    public func unpack() -> Base {
         return self.base
     }
 }
@@ -17,4 +17,21 @@ public protocol Saberable {}
 public extension Saberable {
     var sb: SaberExt<Self> { return SaberExt(self) }
     static var sb: SaberExt<Self>.Type { return SaberExt<Self>.self }
+}
+
+public extension SaberExt where Base == String {
+    func test1() -> Self {
+        Log.info("1111")
+        return self
+    }
+    
+    func test2() -> Self {
+        Log.info("1111")
+        return self
+    }
+    
+    func test3() -> Self {
+        Log.info("3333")
+        return self
+    }
 }
