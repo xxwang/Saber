f6f4ba8ca8b1d45c8a7a7279f50bc7547be69eff
wb <78023776@qq.com>
2022-09-19 10:38:39 +0800

    no message

--- Sources/Saber/Shared/SaberExt.swift
+++ Sources/Saber/Shared/SaberExt.swift
@@ -5,6 +5,7 @@ public class SaberExt<Base> {
     let base: Base
     init(_ base: Base) { self.base = base }
 }
+
 public extension SaberExt {
     /// 获取原始数据
     func unpack() -> Base { return self.base }
@@ -13,10 +14,9 @@ public extension SaberExt {
 // MARK: - 需要使用的类型遵守此协议
 public protocol Saberable {}
 public extension Saberable {
-    var sb: SaberExt<Self> { return SaberExt(self) }
-    static var sb: SaberExt<Self>.Type { return SaberExt<Self>.self }
+    var sb: SaberExt<Self> { SaberExt(self) }
+    static var sb: SaberExt<Self>.Type { SaberExt<Self>.self }
 }
-
 
 // MARK: - 已扩展.sb.属性的类
 extension NSObject: Saberable {}
