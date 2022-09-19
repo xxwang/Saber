2906b1da5abf40f3eb894b232a94803ec2c8dbcc
wb <78023776@qq.com>
2022-09-19 17:04:50 +0800

    no message

--- Sources/Saber/Extensions/Stdlib/OptionalExt.swift
+++ Sources/Saber/Extensions/Stdlib/OptionalExt.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 // MARK: - 属性
-public extension SaberExt where Base == Optional<Any> {
+public extension SaberExt where Base == Any? {
     /// 可选值为空的时候返回 true
     var isNone: Bool {
         switch self.base {
