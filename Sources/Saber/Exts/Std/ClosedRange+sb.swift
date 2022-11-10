13a27fda4cb0053a892627ea0ecdb1c5948bc707
wb <78023776@qq.com>
2022-11-10 17:37:42 +0800

    no message

--- Sources/Saber/Exts/Std/ClosedRange+sb.swift
+++ Sources/Saber/Exts/Std/ClosedRange+sb.swift
@@ -1,12 +1,14 @@
 import Foundation
 
+extension ClosedRange<Int>: Saberable {}
+
 // MARK: - ClosedRange<Int>
-public extension ClosedRange<Int> {
+public extension SaberExt where Base == ClosedRange<Int> {
     /// 转换为索引数组
     /// - Returns:索引数组
     func indexs() -> [Int] {
         var indexs: [Int] = []
-        forEach { index in
+        base.forEach { index in
             indexs.append(index)
         }
         return indexs
