a4782eca3efdba0ea3ce65565bf9aa5ff5298942
wb <78023776@qq.com>
2022-11-22 12:26:23 +0800

    no message

--- Sources/Saber/Exts/Foundation/Notification++.swift
+++ Sources/Saber/Exts/Foundation/Notification++.swift
@@ -1,26 +1,25 @@
 import Foundation
 
-
 extension Notification.Name: Saberable {}
 
-    // MARK: - 方法
+// MARK: - 方法
 public extension SaberExt where Base == Notification.Name {
-        /// 发送通知
-        /// - Parameters:
-        ///   - object:对象
-        ///   - userInfo:携带信息
+    /// 发送通知
+    /// - Parameters:
+    ///   - object:对象
+    ///   - userInfo:携带信息
     func post(
         object: Any? = nil,
         userInfo: [AnyHashable: Any]? = nil
     ) {
         NotificationCenter.sb.post(base, object: object, userInfo: userInfo)
     }
-    
-        /// 添加通知监听(方法)
-        /// - Parameters:
-        ///   - observer:监听者
-        ///   - selector:响应方法
-        ///   - object:对象
+
+    /// 添加通知监听(方法)
+    /// - Parameters:
+    ///   - observer:监听者
+    ///   - selector:响应方法
+    ///   - object:对象
     func add(
         _ observer: Any,
         selector: Selector,
@@ -28,15 +27,15 @@ public extension SaberExt where Base == Notification.Name {
     ) {
         NotificationCenter.sb.add(observer, selector: selector, name: base)
     }
-    
-        /// 移除监听者
-        /// - Parameters:
-        ///   - observer:要移除的监听者
-        ///   - object:对象
+
+    /// 移除监听者
+    /// - Parameters:
+    ///   - observer:要移除的监听者
+    ///   - object:对象
     func remove(
         _ observer: Any,
         object: Any? = nil
     ) {
-        NotificationCenter.sb.remove(observer, name: self.base, object: object)
+        NotificationCenter.sb.remove(observer, name: base, object: object)
     }
 }
