402add8a42d9293ac83e136f109ac3e017c31ebf
wb <78023776@qq.com>
2022-11-29 20:53:21 +0800

    no message

--- Sources/Saber/Saber.swift
+++ Sources/Saber/Saber.swift
@@ -1,11 +1,9 @@
 import UIKit
 
 public struct Saber {
+    
     let text = "Hello, Saber!"
-}
-
-public extension Saber {
-    /// `Hello, Saber!`
+    
     static func sayHello() {
         sb1.info(Saber().text)
     }
